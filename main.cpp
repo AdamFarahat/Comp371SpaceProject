a4936242fc7c5b2771d7d551d07b4c20d1879bd7
Adam Farahat <farahatadam@gmail.com>
2025-07-24 13:38:09 -0400

    Add sphere.h and sphere.cpp

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include "stb_image.h"
 #include "skybox.h"
 #include "camera.h"
+#include "sphere.h"
 
 
 // input handling functions
@@ -99,51 +100,6 @@ GLuint createShaderProgram() {
     return shaderProgram;
 }
 
-void generateSphere(std::vector<float>& vertices, std::vector<unsigned int>& indices, int sectorCount = 36, int stackCount = 18) {
-    float radius = 1.0f;
-    for (int i = 0; i <= stackCount; ++i) {
-        float stackAngle = glm::pi<float>() / 2 - i * glm::pi<float>() / stackCount;
-        float xy = radius * cosf(stackAngle);
-        float z = radius * sinf(stackAngle);
-
-        for (int j = 0; j <= sectorCount; ++j) {
-            float sectorAngle = j * 2 * glm::pi<float>() / sectorCount; //using pi float included with glm 
-            float x = xy * cosf(sectorAngle);
-            float y = xy * sinf(sectorAngle);
-
-            //float u, for sector (horizontal), and v stack (vertical)
-            
-            float u = (float)j / sectorCount;
-            float v = (float)i / stackCount;
-
-
-            vertices.push_back(x);
-            vertices.push_back(y);
-            vertices.push_back(z);
-            vertices.push_back(1.0f);
-            vertices.push_back(1.0f);
-            vertices.push_back(0.0f);
-            //add in the u,v now
-            vertices.push_back(u);
-            vertices.push_back(v);
-
-        }
-    }
-
-    for (int i = 0; i < stackCount; ++i) {
-        for (int j = 0; j < sectorCount; ++j) {
-            int first = i * (sectorCount + 1) + j;
-            int second = first + sectorCount + 1;
-            indices.push_back(first);
-            indices.push_back(second);
-            indices.push_back(first + 1);
-            indices.push_back(second);
-            indices.push_back(second + 1);
-            indices.push_back(first + 1);
-        }
-    }
-}
-
 //Load texture
 GLuint loadTexture(const char* filename)
 {
@@ -154,8 +110,6 @@ GLuint loadTexture(const char* filename)
         std::cerr << "Error::Texture could not load texture file:" << filename << std::endl;
         return 0;
     }
-
-    //return 0; //TODO: replace with texture loading code
 
       //create & bind textures
     GLuint textureId = 0;
